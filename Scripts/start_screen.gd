ca7fcc3407cf11a733d019412e79bdc6b53e8cd7
filipe-ts <filipets@live.com>
2024-05-09 18:42:58 -0300

    Cenas Tela inicial e scorboad

---
+++ Scripts/start_screen.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+	
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/score_board.tscn")
+	
+
+
+
+func _on_button_3_pressed():
+	pass
