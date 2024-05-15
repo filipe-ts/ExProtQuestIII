10106a5cca12155e027b83b9f6981a969394709e
Marcellomrg <125364232+Marcellomrg@users.noreply.github.com>
2024-05-15 18:38:29 -0300

    Add files via upload

---
+++ Scenes/score_board.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+
+
+func _ready():
+	pass
+
+
+
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	pass
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/start_screen.tscn")
---
+++ Scenes/start_screen.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+	
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/score_board.tscn")
+	
+
+
+
+func _on_button_3_pressed():
+	pass
+
+
+func _on_button_4_pressed():
+	get_tree().change_scene_to_file("res://Scenes/config.tscn")
