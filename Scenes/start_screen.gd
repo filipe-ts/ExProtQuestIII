adce69be33365ea3d5b3ba8d530efea2ff8d6ca9
filipe-ts <filipets@live.com>
2024-06-12 10:24:01 -0300

    Adicionado Botao Sair

--- Scenes/start_screen.gd
+++ Scenes/start_screen.gd
@@ -54,4 +54,7 @@ func _on_button_4_pressed():
 	get_tree().root.add_child(instance)
 	queue_free()
 
-
+func _on_btn_sair_pressed():
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit(0)
+	
